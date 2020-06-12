a8a597960637500c5935f8304907715bc23b988a
maneeshpm <manu.pm55@gmail.com>
2020-06-12 17:34:57 +0530

    integating click cli interface

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
-from auth import auth
-from downloader import downloader
+from auth import Auth
+from DownloadManager import DownloadManager
+import click
 
 SCOPES = ['https://www.googleapis.com/auth/drive']
-secretCred = 'credentials.json'
+clientSecretKey = 'credentials.json'
 
-newAuth = auth(SCOPES, secretCred)
-cred = newAuth.getAuth()
+newAuth = Auth(clientSecretKey, SCOPES)
+newCreds = newAuth.getCredentials()
+newDownloadManager = DownloadManager(newCreds)
 
-newDownloader = downloader(cred)
-newDownloader.createService()
+@click.group()
+def main():
+    """Script to download files from google drive using Google Drive API"""
+    pass
 
-newDownloader.getList()
-newDownloader.downloadFromList()
-# newDownloader.download('1xf9SigkYdGjhY3rA0TBBquQWrlLZA9kQ::Screenshot from 2020-04-13 12-50-45.png,\n')
+@main.command()
+def gl():
+    """Generate a list of all items in your drive as a CSV file"""
+    newDownloadManager.getList()
+
+@main.command()
+def td():
+    """Download items specified in itemList.csv with threading"""
+    newDownloadManager.downloadFromListThread('itemList.csv')
+
+@main.command()
+def rd():
+    """Download items specified in itemList.csv without threading"""
+    newDownloadManager.downloadFromListRegular('itemList.csv')
+
+if __name__ == '__main__':
+    main()
