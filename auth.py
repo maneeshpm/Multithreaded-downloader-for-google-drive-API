09df9bf2c94194d6a2747a5293dd97eb6c2eee8b
maneeshpm <manu.pm55@gmail.com>
2020-06-12 17:34:36 +0530

    authentication module now first check for a token in the current directory

--- auth.py
+++ auth.py
@@ -1,32 +1,27 @@
-from __future__ import print_function
-import io
 import pickle
-import os.path
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+import os.path
 
-class auth():
-    def __init__(self, scope, secretCred):
-        self.scope = scope
-        self.secretCred = secretCred
-    
-    def getAuth(self):
+class Auth:
+
+    def __init__(self, clientSecretKeyFile, SCOPES):
         self.creds = None
+        self.clientSecretKeyFile = clientSecretKeyFile
+        self.SCOPES = SCOPES
+
+    def getCredentials(self):    
         if os.path.exists('token.pickle'):
             with open('token.pickle', 'rb') as token:
-                self.creds = pickle.load(token)
-        
-        if not self.creds or not self.creds.valid:
-            if self.creds and self.creds.expired and self.creds.refresh_token:
-                self.creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    self.secretCred, self.scope)
-                self.creds = flow.run_local_server(port=0)
-            
-            with open('token.pickle', 'wb') as token:
-                pickle.dump(self.creds, token)
-        
-        return self.creds
+                creds = pickle.load(token)
 
-    
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(self.clientSecretKeyFile, self.SCOPES)
+                creds = flow.run_local_server(port=0)
+
+                with open('token.pickle','wb') as token:
+                    pickle.dump(creds, token)
+        return creds
